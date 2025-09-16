221b132bd37f263a1de60ad7773a2d907efd164f
Palak_Soni <sonipalak2018@gmail.com>
2025-09-16 23:20:18 +0530

    Update Multiple Inheritance.cpp

--- Multiple Inheritance.cpp
+++ Multiple Inheritance.cpp
@@ -1,1 +1,49 @@
+//EXPERIMENT 14
+//Name:- Palak Soni
+//Class:-ENTC A
+//PRN:- 24070123069
+//Problem Statement:- Write a code to show Multiple Inheritance.
 
+#include <iostream>
+using namespace std;
+
+class Employee {
+protected:
+    string name;
+    int id;
+public:
+    void setEmployee(string n, int i) {
+        name = n;
+        id = i;
+    }
+};
+
+class Salary {
+protected:
+    double salary;
+public:
+    void setSalary(double s) {
+        salary = s;
+    }
+};
+
+class Manager : public Employee, public Salary {
+public:
+    void display() {
+        cout << "Manager Name: " << name << "\n ID: " << id << "\n Salary: Rs " << salary << endl;
+    }
+};
+
+int main() {
+    Manager m;
+    m.setEmployee("Palak Soni", 101);
+    m.setSalary(75000);
+    m.display();
+    return 0;
+}
+
+/*Output:-
+Manager Name: Palak Soni
+ID: 101
+Salary: Rs 75000
+*/
